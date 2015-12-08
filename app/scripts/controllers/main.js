8f0c9fcc2cec8691eb4e3a3d5f0b801e8f1d19e0
rajive <rajinhoeduardo@gmail.com>
2015-12-08 22:35:54 +0000

    nm

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,24 +11,29 @@
         $scope.totalPrice = 0;
         $scope.modal = false;
 
+        // Fetch JSON data using get factory
         dataFactory.get().then(function (data) {
             $scope.products = data['products'];
         });
 
+        // Triggered when any 'Add to Cart' button clicked. Use array index as id
         $scope.addToCart = function(id, product) {
-            //Use array position as id
             if (!cartItems[id]) {
+                // Add new item to cartItems Object
                 cartItems[id] = {
                     'product': product,
                     'total': 1
                 };
             } else {
+                //Increment chosen item total by 1
                 cartItems[id]['total'] ++;
             }
+            // Always increment total items in the cart and calculate total price
             $scope.totalItems ++;
             $scope.totalPrice += parseFloat(product['price']);
         }
 
+        // Triggered when cart button clicked
         $scope.showModal = function(bool) {
             $scope.modal = bool;
         }
---
+++ readme.txt
@@ -0,0 +1,11 @@
+Hopefully it's visually intuitive to use however it may not be obvious where the relevant files are...
+
+--- JavaScript ---
+scripts/app.js
+scripts/controllers/main.js
+
+--- HTML ---
+app/views/main.html
+
+--- CSS (SASS Compiled via Grunt.js) ---
+styles/main.scss
