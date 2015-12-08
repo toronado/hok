06664989110e3f7c0b80feefb24fa37c2662f942
toronado <rajiv@Rajivs-MacBook-Pro.local>
2015-12-08 17:10:30 +0000

    nm

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,26 +1,36 @@
 'use strict';
 
 /*
-=== Globals ===
-*/
-
-
-/*
 === Controller: Main ===
 */
     hokApp.controller('MainCtrl', function ($scope, dataFactory) {
 
-        $scope.total = 0;
-        $scope.items = [];
+        var cartItems = {};
+        $scope.cartItems = cartItems;
+        $scope.totalItems = 0;
+        $scope.totalPrice = 0;
+        $scope.modal = false;
 
         dataFactory.get().then(function (data) {
             $scope.products = data['products'];
         });
 
-        $scope.addToCart = function(id, item) {
-            //console.log(id);
-            $scope.items.push(item);
-            $scope.total += parseFloat(item['price']);
+        $scope.addToCart = function(id, product) {
+            //Use array position as id
+            if (!cartItems[id]) {
+                cartItems[id] = {
+                    'product': product,
+                    'total': 1
+                };
+            } else {
+                cartItems[id]['total'] ++;
+            }
+            $scope.totalItems ++;
+            $scope.totalPrice += parseFloat(product['price']);
+        }
+
+        $scope.showModal = function(bool) {
+            $scope.modal = bool;
         }
 
     });
